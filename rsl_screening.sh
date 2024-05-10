e29a9fe6608b945b1d03e2443696a3ca5ab5e365
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-05-10 15:55:31 +0900

    update Resolve Rise Time Screening

--- rsl_screening.sh
+++ rsl_screening.sh
@@ -12,7 +12,11 @@ export PFILES="./pfiles;$HEADAS/syspfiles"
 obsid=$1
 
 #ftcopy infile="${obsid}rsl_p0px1000_cl.evt.gz[EVENTS][(PI>=600)&&((RISE_TIME>=40&&RISE_TIME<=60&&ITYPE<4)||(ITYPE==4))&&STATUS[4]==b0]" outfile=${obsid}rsl_p0px1000_cl2.evt copyall=yes clobber=yes history=yes
+
 #for above 15 keV
-ftcopy infile="${obsid}rsl_p0px1000_cl.evt[EVENTS][(PI>=600)&&((RISE_TIME>=30&&RISE_TIME<=60&&ITYPE<4)||(ITYPE==4))&&STATUS[4]==b0]" outfile=${obsid}rsl_p0px1000_cl2.evt copyall=yes clobber=yes history=yes
+#ftcopy infile="${obsid}rsl_p0px1000_cl.evt.gz[EVENTS][(PI>=600)&&((RISE_TIME>=30&&RISE_TIME<=60&&ITYPE<4)||(ITYPE==4))&&STATUS[4]==b0]" outfile=${obsid}rsl_p0px1000_cl2.evt copyall=yes clobber=yes history=yes
+
+#XRISM Quick-Start Guide Version 2.1
+ftcopy infile="${obsid}rsl_p0px1000_cl.evt.gz[EVENTS][(PI>=600) && (((((RISE_TIME+0.00075*DERIV_MAX)>46)&&((RISE_TIME+0.00075*DERIV_MAX)<58))&&ITYPE<4)||(ITYPE==4))&&STATUS[4]==b0]" outfile=${obsid}rsl_p0px1000_cl2.evt copyall=yes clobber=yes history=yes
 
 rm -fr pfiles
