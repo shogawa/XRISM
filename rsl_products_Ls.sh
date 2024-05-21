82424d1efc81c2ecb406c8166fed0d4871263e14
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-05-22 04:42:58 +0900

    added: exclude Ls events

---
+++ rsl_products_Ls.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+export HEADAS=/home/ogawa/work/tools/heasoft/XRISM_15Oct2023_Build7/x86_64-pc-linux-gnu-libc2.31
+. $HEADAS/headas-init.sh
+
+export CALDB=/home/ogawa/work/tools/caldb
+. $CALDB/software/tools/caldbinit.sh
+
+export XSELECT_MDB=/home/ogawa/work/analysis/xrism/xselect.mdb.xrism
+
+dir_analysis=$1
+obsid=$2
+dir_scripts=`pwd`
+cd $dir_analysis
+#sh $dir_scripts/rsl_telgti_screen.sh $obsid
+sh $dir_scripts/rsl_copy.sh $obsid
+sh $dir_scripts/rsl_screening_Ls.sh $obsid
+sh $dir_scripts/rsl_imgextract.sh $obsid
+sh $dir_scripts/rsl_specextract.sh $obsid
+sh $dir_scripts/rsl_lcextract.sh $obsid 128
+sh $dir_scripts/rsl_rmf.sh $obsid
+sh $dir_scripts/rsl_xaexpmap.sh $obsid
+sh $dir_scripts/rsl_xaarfgen.sh $obsid ${obsid}rsl_pix_S.rmf region_RSL_det_27.reg
+sh $dir_scripts/grppha.sh ${obsid}rsl_pix.pha ${obsid}rsl_pixgr1.pha min 1
+sh $dir_scripts/bkg_rmf_arf.sh ${obsid}rsl_pixgr1.pha NONE ${obsid}rsl_pix_S.rmf ${obsid}rsl_p0px1000_ptsrc.arf
---
+++ rsl_screening_Ls.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export HEADAS=/home/ogawa/work/tools/heasoft/XRISM_15Oct2023_Build7/x86_64-pc-linux-gnu-libc2.31
+. $HEADAS/headas-init.sh
+
+export CALDB=/home/ogawa/work/tools/caldb
+. $CALDB/software/tools/caldbinit.sh
+
+pfiles_dir=pfiles
+mkdir -p $pfiles_dir
+export PFILES="`pwd`/${pfiles_dir};$HEADAS/syspfiles"
+
+obsid=$1
+
+#XRISM Quick-Start Guide Version 2.1
+#||(ITYPE==4))&&STATUS[4]==b0 is removed i.e., Ls events are removed
+ftcopy infile="${obsid}rsl_p0px1000_cl.evt.gz[EVENTS][(PI>=600) && ((((RISE_TIME+0.00075*DERIV_MAX)>46)&&((RISE_TIME+0.00075*DERIV_MAX)<58))&&ITYPE<4)]" outfile=${obsid}rsl_p0px1000_cl2.evt copyall=yes clobber=yes history=yes
+
+rm -fr $pfiles_dir
