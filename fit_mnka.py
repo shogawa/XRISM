b8e022f3ec1e10187765f9ad4f7ad166488d973d
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-05-30 11:01:35 +0900

    modified offset calculation

--- fit_mnka.py
+++ fit_mnka.py
@@ -100,19 +100,17 @@ xs.Fit.error("1. 1,27")
 xs.Fit.error("1. 2",True)
 
 errn = m1.gsmooth.Sig_6keV.values[0] - m1.gsmooth.Sig_6keV.error[0]
-errn = errn*1e3
 errp = m1.gsmooth.Sig_6keV.error[1] - m1.gsmooth.Sig_6keV.values[0]
-errp = errp*1e3
-value = m1.gsmooth.Sig_6keV.values[0]*1e3
-fwhm = np.array([value, errp, errn])
+value = m1.gsmooth.Sig_6keV.values[0]
+fwhm = np.array([value, errp, errn])*1000
 print("sigma: {0:.3f}+{1:.3f}-{2:.3f}".format(value, errp, errn))
 print("FWHM: {0:.3f}+{1:.3f}-{2:.3f}".format(value*2.35, errp*2.35, errn*2.35))
 
 errn = resp.gain.offset.values[0] - resp.gain.offset.error[0]
 errp = resp.gain.offset.error[1] - resp.gain.offset.values[0]
 value = resp.gain.offset.values[0]
-offset = np.array([value, errp, errn])
-print("{0}+{1}-{2}".format(value, errp, errn))
+offset = np.array([value, errp, errn])*1000
+print("offset: {0}+{1}-{2}".format(value, errp, errn))
 
 plot_data = plot_parameters("data del")
 
@@ -148,7 +146,7 @@ color=cmp(0)
 
 txt = target.replace('GC', 'GC ').replace('IC', 'IC ').replace('-', '$-$').replace('CenA', 'Centaurus A')
 txt += "\nFWHM: ${0:.3f}^{{+{1:.3f}}}_{{-{2:.3f}}}$".format(*fwhm*2.35)
-txt += "\noffset: ${0:.3f}^{{+{1:.3f}}}_{{-{2:.3f}}}$".format(*offset*2.35)
+txt += "\noffset: ${0:.3f}^{{+{1:.3f}}}_{{-{2:.3f}}}$".format(*offset)
 axes[0].text(0.01, 0.70, txt, transform=axes[0].transAxes, size=12)
 
 
