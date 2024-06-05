caca6ae0c7ecffc5212f9f3f98120b259173360b
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-06-05 18:59:02 +0900

    mkdir resolve

--- resolve/rsl_products.sh
+++ resolve/rsl_products.sh
@@ -11,6 +11,7 @@ export XSELECT_MDB=/home/ogawa/work/analysis/xrism/xselect.mdb.xrism
 dir_analysis=$1
 obsid=$2
 dir_scripts=`pwd`
+dir_scripts_comon=$dir_scripts/..
 cd $dir_analysis
 #sh $dir_scripts/rsl_telgti_screen.sh $obsid
 sh $dir_scripts/rsl_copy.sh $obsid
@@ -21,5 +22,5 @@ sh $dir_scripts/rsl_lcextract.sh $obsid 128
 sh $dir_scripts/rsl_rmf.sh $obsid
 sh $dir_scripts/rsl_xaexpmap.sh $obsid
 sh $dir_scripts/rsl_xaarfgen.sh $obsid ${obsid}rsl_pix_S.rmf region_RSL_det_27.reg
-sh $dir_scripts/grppha.sh ${obsid}rsl_pix.pha ${obsid}rsl_pixgr1.pha min 1
-sh $dir_scripts/bkg_rmf_arf.sh ${obsid}rsl_pixgr1.pha NONE ${obsid}rsl_pix_S.rmf ${obsid}rsl_p0px1000_ptsrc.arf
+sh $dir_scripts_comon/grppha.sh ${obsid}rsl_pix.pha ${obsid}rsl_pixgr1.pha min 1
+sh $dir_scripts_comon/bkg_rmf_arf.sh ${obsid}rsl_pixgr1.pha NONE ${obsid}rsl_pix_S.rmf ${obsid}rsl_p0px1000_ptsrc.arf
