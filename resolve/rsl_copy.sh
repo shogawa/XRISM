6397909a3565ab5018c995fcb9cabc3e7a1d8ecf
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-06-05 19:18:36 +0900

    update

--- resolve/rsl_copy.sh
+++ resolve/rsl_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 obsid=$1
-ln -si ../resolve/event_cl/${obsid}rsl_p0px1* .
+ln -sf ../resolve/event_cl/${obsid}rsl_p0px1* .
 ln -sf ../resolve/event_uf/${obsid}rsl_px1000_exp.gti.gz .
 ln -sf ../auxil/${obsid}.ehk.gz .
 
