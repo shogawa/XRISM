1ed39466949dcb50a4c63662e7de7e94950cfb1a
shogawa <ogawa@papagena.kusastro.kyoto-u.ac.jp>
2024-07-14 10:25:29 +0900

    update

--- shellscripts/activate.sh
+++ shellscripts/activate.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-export TOOLS=/home/ogawa/work/tools
-
-export HEADAS=$TOOLS/heasoft/XRISM_20Jun2024_Build8/x86_64-pc-linux-gnu-libc2.31
+export HEADAS=/home/ogawa/work/tools/heasoft/XRISM_20Jun2024_Build8/x86_64-pc-linux-gnu-libc2.31
 
 . $HEADAS/headas-init.sh
 
-export CALDB=$TOOLS/caldb
+export CALDB=/home/ogawa/work/tools/caldb
 . $CALDB/software/tools/caldbinit.sh
 
-export XSELECT_MDB=$TOOLS/heasoft/xrism/xselect.mdb.xrism
+export XSELECT_MDB=/home/ogawa/work/tools/heasoft/xrism/xselect.mdb.xrism
